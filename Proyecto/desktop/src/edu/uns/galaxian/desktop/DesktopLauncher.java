cb883f4a98de13d245748a8b43723d11183cc503
nicoverali <nicolasalevera98@gmail.com>
2018-09-08 16:49:33 -0300

    Agregado proyecto del juego.
    Además de la configuración inicial, se añadio la librería "JSON in Java" - {https://github.com/stleary/JSON-java}

---
+++ Proyecto/desktop/src/edu/uns/galaxian/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.uns.galaxian.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.uns.galaxian.Juego;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Juego(), config);
+	}
+}
